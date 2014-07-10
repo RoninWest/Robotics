d6f899de1da23f64ebc1fdb327768963d5916523
Huy Nguyen <huy@ronin.ws>
2014-07-09 20:44:31 -0700

    change to v4.51 .net runtime & add readme note

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Robotics
 ========
+
+This code rely mostly on the EV3.NET library here: https://legoev3.codeplex.com/
--- Ronin.Robotics.Test/Properties/AssemblyInfo.cs
+++ Ronin.Robotics.Test/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Ronin.Robotics.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Ronin West Consulting 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
