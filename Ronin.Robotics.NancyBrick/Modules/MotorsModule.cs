c865109fb91aa839d06b7690c466ca50a0682b45
Huy Nguyen <huy@ronin.ws>
2014-07-29 03:28:37 -0700

    got soft deugger setup but it is not working with mono brick :( nancy is a failed experiement

--- Ronin.Robotics.NancyBrick/Modules/MotorsModule.cs
+++ Ronin.Robotics.NancyBrick/Modules/MotorsModule.cs
@@ -2,6 +2,7 @@
 using Nancy;
 using MonoBrickFirmware;
 using MonoBrickFirmware.Sound;
+using MonoBrickFirmware.Display;
 
 namespace Ronin.Robotics.NancyBrick
 {
@@ -10,10 +11,13 @@ namespace Ronin.Robotics.NancyBrick
 		public MotorsModule ()
 		{
 			Get ["/"] = a => {
+				LcdConsole.WriteLine("Hello C0nsole from NancyFX!");
+
 				var s = new Speaker(50);
 				s.Beep(200);
 				s.Buzz(200);
-				return "Hellow from NancyFX!";
+
+				return "Hell0w REST client from NancyFX!";
 			};
 		}
 	}
